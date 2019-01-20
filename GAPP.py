594d1879ede880b4e17d18a96b9dc0ed1cd50ed8
Whompratt <JakeR1342@gmail.com>
2019-01-20 21:53:36 +0000

    Added pit stop strategy information (more to do here) as well as some basic security stuff.

--- GAPP.py
+++ GAPP.py
@@ -4,12 +4,13 @@ import requests
 import re
 import csv
 import collections
+import math
 from lxml import html
 from lxml import etree
 
 
 # Data collection function
-def collection(username, password, weather, temp):
+def collection(username, password, weather, temp, wear, clearTrackRisk):
 	token = "9da482f717cf1319f10f55e35ab767a5"
 	logon = "Login"
 	logonFake = "Sign in"
@@ -30,10 +31,15 @@ def collection(username, password, weather, temp):
 		driver_url = "https://gpro.net/gb/" + driverID[0]
 	except:
 		return [0, 0, 0, 0, 0, 0]
+	teamName = tree.xpath("//a[starts-with(@href, 'TeamProfile.asp')]/text()")
+	if(teamName[0] != "VIPER AUTOSPORT") and (teamName[0] != "TEAM VIPER") and (teamName[0] != "VIPER RACING"):
+		return [1, 0, 0, 0, 0, 0]
 	trackID = tree.xpath("//a[starts-with(@href, 'TrackDetails.asp')]/@href")
 	track_url = "https://gpro.net/gb/" + trackID[0]
 	car_url = "https://www.gpro.net/gb/UpdateCar.asp"
 	race_url = "https://www.gpro.net/gb/RaceSetup.asp"
+	managerID = tree.xpath("//a[contains(@href, 'viewnation')]/../a[2]/@href")
+	manager_url = "https://gpro.net/gb/" + managerID[0]
 	
 	
 	# Request the driver information page and scrape the driver data
@@ -51,7 +57,13 @@ def collection(username, password, weather, temp):
 	driverRep = int(tree.xpath("normalize-space(//tr[contains(@data-step, '13')]//td/text())"))
 	driverWei = int(tree.xpath("normalize-space(//tr[contains(@data-step, '14')]//td/text())"))
 	driverAge = int(tree.xpath("normalize-space(//tr[contains(@data-step, '15')]//td/text())"))
-	
+
+
+	# Request the manager page and scrape tyre data
+	managerResult = session.get(manager_url, headers = dict(referer = manager_url))
+	tree = html.fromstring(managerResult.content)
+	tyreSupplier = str(tree.xpath("//img[contains(@src, 'suppliers')]/@alt")[0])
+
 	
 	# Request the track information page and scrape the track data
 	trackResult = session.get(track_url, headers=dict(referer=track_url))
@@ -94,7 +106,7 @@ def collection(username, password, weather, temp):
 	RtempMaxFou = int((re.findall("\d+", RtempRangeFou))[1])
 	# Find the averages of these temps for the setup
 	Rtemp = ((RtempMinOne + RtempMaxOne) + (RtempMinTwo + RtempMaxTwo) + (RtempMinThr + RtempMaxThr) + (RtempMinFou + RtempMaxFou)) / 8
-	
+
 	
 	# Using the race strategy page requested earlier, scrape the qualifying weather data
 	QOneTemp = str(tree.xpath("normalize-space(//img[contains(@name, 'WeatherQ')]/../text()[contains(., 'Temp')])"))
@@ -292,14 +304,34 @@ def collection(username, password, weather, temp):
 
 	setup = [int(setupFWi), int(setupRWi), int(setupEng), int(setupBra), int(setupGea), int(setupSus)]
 
-	#frontWing.set(str(setup[0]))
-	#rearWing.set(str(setup[1]))
-	#engine.set(str(setup[2]))
-	#brakes.set(str(setup[3]))
-	#gear.set(str(setup[4]))
-	#suspension.set(str(setup[5]))
+	tyreBrand = {"Pipirelli": 1, "Avonn": 8, "Yokomama": 3, "Dunnolop": 4, "Contimental": 8, "Badyear": 7}
+	trackWearLevel = {"Very low": 0, "Low": 1, "Medium": 2, "High": 3, "Very high": 4}
+
+	wearFactors = [0.998163750229071, 0.997064844817654, 0.996380346554349, 0.995862526048112, 0.996087854384523]
+
+	for i in range(5):
+		stops[i].set(str(stopCalc(trackDst, trackWearLevel[trackTyr], Rtemp, tyreBrand[tyreSupplier], i, carLevelSus, driverAgg, driverExp, driverWei, clearTrackRisk, float(trackData[trackNam][9]), wear, wearFactors[i])))
 
 	return setup
+
+# Pit Stop Calc
+# trackDst = Track Distance
+# tracWearLevel = Very Low, Low, Medium, High, Very High, and it's relating factor, 0, 1, 2, 3, 4 respectively
+# Rtemp = Race Temperature
+# tyreBrand = Tyre Brand Factor, 1 for Pipirello, 8 for Avonn, etc.
+# tyreType = Tyre Compound Factor, 0.998163750229071 for Extra Soft (look at wearFactors)
+# carLevelSus = Suspension Level equipped to car
+# driverAgg = Driver Aggressiveness
+# driverExp = Driver Experience
+# driverWeight = Driver Weight
+# clearTrackRisk = Clear Track Risk used, as a percentage
+# trackBaseWear = Track Base Wear from trackData.csv
+# wearLimit = The manager chosen limit for tyre wear before pitting, so at 10%, we assume the stint will end when the tyres hit 10% wear
+def stopCalc(trackDst, trackWearLevel, Rtemp, tyreBrand, tyreType, carLevelSus, driverAgg, driverExp, driverWeight, clearTrackRisk, trackBaseWear, wearLimit, tyreWearFactor):
+	baseWear = 129.776458172062
+	productFactors = (0.896416176238624 ** trackWearLevel) * (0.988463622 ** Rtemp) * (1.048876356 ** tyreBrand) * (1.355293715 ** tyreType) * (1.009339294 ** carLevelSus) * (0.999670155 ** driverAgg) * (1.00022936 ** driverExp) * (0.999858329 ** driverWeight) * (tyreWearFactor ** (clearTrackRisk / 100))
+	stops = math.ceil((trackDst) / ((productFactors * (tyreWearFactor ** clearTrackRisk) * baseWear * trackBaseWear) * ((100 - wearLimit) / 100))) - 1
+	return stops
 
 
 # Warning window
@@ -317,8 +349,25 @@ def calculate(*args):
 		password = str(inputPassword.get())
 		weather = str(inputWeather.get())
 		session = str(inputSession.get())
-		setup = collection(username, password, weather, session)
-		if(setup[0] != 0):
+		try:
+			wear = float(re.findall('\d+.\d+', inputWear.get())[0])
+		except:
+			try:
+				wear = float(re.findall('\d+', inputWear.get())[0])
+			except:
+				wear = 0.0
+		try:
+			clearTrackRisk = int(re.findall('\d+', inputCTR.get())[0])
+		except:
+			clearTrackRisk = 0
+
+		setup = collection(username, password, weather, session, wear, clearTrackRisk)
+
+		if(setup[0] == 0):
+			warningLabel.set("Incorect Login Details")
+		elif(setup[0] == 1):
+			warningLabel.set("VIPER Family Team Only")
+		else:
 			warningLabel.set("")
 			frontWing.set(str(setup[0]))
 			rearWing.set(str(setup[1]))
@@ -326,8 +375,6 @@ def calculate(*args):
 			brakes.set(str(setup[3]))
 			gear.set(str(setup[4]))
 			suspension.set(str(setup[5]))
-		else:
-			warningLabel.set("Incorect Login Details")
 		
 	except ValueError:
 		pass
@@ -339,10 +386,16 @@ root.title("GAPP")
 
 
 # and build the frame
-mainframe = ttk.Frame(root, padding = "3 3 12 12")
-mainframe.grid(column = 0, row = 0, sticky = (N, W, E, S))
+frameSetup = ttk.LabelFrame(root, padding = "3 3 12 12")
+frameSetup.grid(column = 0, row = 0, sticky = (N, W, E, S))
+frameStrategy = ttk.LabelFrame(root, padding = "3 3 12 12")
+frameStrategy.grid(column = 1, row = 0, sticky = (N, W, E))
 root.columnconfigure(0, weight = 1)
 root.rowconfigure(0, weight = 1)
+frameSetup.columnconfigure(0, weight = 1)
+frameSetup.rowconfigure(0, weight = 1)
+frameStrategy.columnconfigure(0, weight = 1)
+frameStrategy.rowconfigure(0, weight = 1)
 
 
 # Declare our userful variables
@@ -354,6 +407,11 @@ inputTemp = IntVar()
 inputSession = StringVar()
 inputSession.set("Race")
 warningLabel = StringVar()
+
+inputWear = StringVar()
+inputWear.set("20")
+inputCTR = StringVar()
+inputCTR.set("0")
 
 frontWing = StringVar()
 rearWing = StringVar()
@@ -372,54 +430,142 @@ suspension.set("0")
 
 # INPUT
 # Build the entry boxes
-entryUsername = ttk.Entry(mainframe, width = 30, textvariable = inputUsername)
+entryUsername = ttk.Entry(frameSetup, width = 30, textvariable = inputUsername)
 entryUsername.grid(column = 2, row = 1, sticky = (W, E))
 
-entryPassword = ttk.Entry(mainframe, width = 30, show = "*", textvariable = inputPassword)
+entryPassword = ttk.Entry(frameSetup, width = 30, show = "*", textvariable = inputPassword)
 entryPassword.grid(column = 2, row = 2, sticky = (W, E))
 
-radioQ1 = ttk.Radiobutton(mainframe, text = "Q1", variable = inputSession, value = "Q1").grid(column = 2, row = 3, sticky = (W, E))
-radioQ2 = ttk.Radiobutton(mainframe, text = "Q2", variable = inputSession, value = "Q2").grid(column = 2, row = 4, sticky = (W, E))
-radioRace = ttk.Radiobutton(mainframe, text = "Race", variable = inputSession, value = "Race").grid(column = 2, row = 5, sticky = (W, E))
+radioQ1 = ttk.Radiobutton(frameSetup, text = "Q1", variable = inputSession, value = "Q1").grid(column = 2, row = 3, sticky = (W, E))
+radioQ2 = ttk.Radiobutton(frameSetup, text = "Q2", variable = inputSession, value = "Q2").grid(column = 2, row = 4, sticky = (W, E))
+radioRace = ttk.Radiobutton(frameSetup, text = "Race", variable = inputSession, value = "Race").grid(column = 2, row = 5, sticky = (W, E))
 
-radioDry = ttk.Radiobutton(mainframe, text = "Dry", variable = inputWeather, value = "Dry")
+radioDry = ttk.Radiobutton(frameSetup, text = "Dry", variable = inputWeather, value = "Dry")
 radioDry.grid(column = 2, row = 6, sticky = (W, E))
-radioWet = ttk.Radiobutton(mainframe, text = "Wet", variable = inputWeather, value = "Wet")
+radioWet = ttk.Radiobutton(frameSetup, text = "Wet", variable = inputWeather, value = "Wet")
 radioWet.grid(column = 2, row = 7, sticky = (W, E))
 
 
 # Add labels to the entry boxes
-ttk.Label(mainframe, text = "Username: ").grid(column = 1, row = 1, sticky = (W, E))
-ttk.Label(mainframe, text = "Password: ").grid(column = 1, row = 2, sticky = (W, E))
-ttk.Label(mainframe, text = "Session: ").grid(column = 1, row = 3, sticky = (W, E))
-ttk.Label(mainframe, text = "Weather: ").grid(column = 1, row = 6, sticky = (W, E))
+ttk.Label(frameSetup, text = "Username: ").grid(column = 1, row = 1, sticky = (W, E))
+ttk.Label(frameSetup, text = "Password: ").grid(column = 1, row = 2, sticky = (W, E))
+ttk.Label(frameSetup, text = "Session: ").grid(column = 1, row = 3, sticky = (W, E))
+ttk.Label(frameSetup, text = "Weather: ").grid(column = 1, row = 6, sticky = (W, E))
 
 
 # Add a button to calculate the setup
-ttk.Button(mainframe, text = "Calculate", command = calculate).grid(column = 1, row = 8)
-ttk.Label(mainframe, textvariable = warningLabel).grid(column = 2, row = 8)
+ttk.Button(frameSetup, text = "Calculate", command = calculate).grid(column = 1, row = 8)
+ttk.Label(frameSetup, textvariable = warningLabel).grid(column = 2, row = 8)
 
 
 
 # OUTPUT
-ttk.Label(mainframe, text = "Front Wing: ").grid(column = 1, row = 9, sticky = (W, E))
-ttk.Label(mainframe, text = "Rear Wing: ").grid(column = 1, row = 10, sticky = (W, E))
-ttk.Label(mainframe, text = "Engine: ").grid(column = 1, row = 11, sticky = (W, E))
-ttk.Label(mainframe, text = "Brakes: ").grid(column = 1, row = 12, sticky = (W, E))
-ttk.Label(mainframe, text = "Gear: ").grid(column = 1, row = 13, sticky = (W, E))
-ttk.Label(mainframe, text = "Suspension: ").grid(column = 1, row = 14, sticky = (W, E))
+ttk.Label(frameSetup, text = "Front Wing: ").grid(column = 1, row = 9, sticky = (W, E))
+ttk.Label(frameSetup, text = "Rear Wing: ").grid(column = 1, row = 10, sticky = (W, E))
+ttk.Label(frameSetup, text = "Engine: ").grid(column = 1, row = 11, sticky = (W, E))
+ttk.Label(frameSetup, text = "Brakes: ").grid(column = 1, row = 12, sticky = (W, E))
+ttk.Label(frameSetup, text = "Gear: ").grid(column = 1, row = 13, sticky = (W, E))
+ttk.Label(frameSetup, text = "Suspension: ").grid(column = 1, row = 14, sticky = (W, E))
 
-ttk.Label(mainframe, textvariable = frontWing).grid(column = 2, row = 9)
-ttk.Label(mainframe, textvariable = rearWing).grid(column = 2, row = 10)
-ttk.Label(mainframe, textvariable = engine).grid(column = 2, row = 11)
-ttk.Label(mainframe, textvariable = brakes).grid(column = 2, row = 12)
-ttk.Label(mainframe, textvariable = gear).grid(column = 2, row = 13)
-ttk.Label(mainframe, textvariable = suspension).grid(column = 2, row = 14)
+ttk.Label(frameSetup, textvariable = frontWing).grid(column = 2, row = 9)
+ttk.Label(frameSetup, textvariable = rearWing).grid(column = 2, row = 10)
+ttk.Label(frameSetup, textvariable = engine).grid(column = 2, row = 11)
+ttk.Label(frameSetup, textvariable = brakes).grid(column = 2, row = 12)
+ttk.Label(frameSetup, textvariable = gear).grid(column = 2, row = 13)
+ttk.Label(frameSetup, textvariable = suspension).grid(column = 2, row = 14)
 
 
+# Strategy
+ttk.Label(frameStrategy, text = "Wear:", padding = "5 10").grid(column = 1, row = 1, sticky = (W, E))
+entryWear = ttk.Entry(frameStrategy, width = 10, textvariable = inputWear).grid(column = 2, row = 1, sticky = (W, E))
+ttk.Label(frameStrategy, text = "CTR:", padding = "5 10").grid(column = 3, row = 1, sticky = (W, E))
+entryCTR = ttk.Entry(frameStrategy, width = 10, textvariable = inputCTR).grid(column = 4, row = 1, sticky = (W, E))
+
+ttk.Label(frameStrategy, text = "Tyre", padding = "5 10").grid(column = 1, row = 2, sticky = (W, E))
+ttk.Label(frameStrategy, text = "Stops", padding = "5 10").grid(column = 2, row = 2, sticky = (W, E))
+ttk.Label(frameStrategy, text = "Fuel Load", padding = "5 10").grid(column = 3, row = 2, sticky = (W, E))
+ttk.Label(frameStrategy, text = "Pit Time", padding = "5 10").grid(column = 4, row = 2, sticky = (W, E))
+ttk.Label(frameStrategy, text = "Compound Loss", padding = "5 10").grid(column = 5, row = 2, sticky = (W, E))
+ttk.Label(frameStrategy, text = "Fuel Loss", padding = "5 10").grid(column = 6, row = 2, sticky = (W, E))
+ttk.Label(frameStrategy, text = "Pit Total", padding = "5 10").grid(column = 7, row = 2, sticky = (W, E))
+ttk.Label(frameStrategy, text = "Total", padding = "5 10").grid(column = 8, row = 2, sticky = (W, E))
+
+ttk.Label(frameStrategy, text = "Extra Soft").grid(column = 1, sticky = (W, E))
+ttk.Label(frameStrategy, text = "Soft").grid(column = 1, sticky = (W, E))
+ttk.Label(frameStrategy, text = "Medium").grid(column = 1, sticky = (W, E))
+ttk.Label(frameStrategy, text = "Hard").grid(column = 1, sticky = (W, E))
+ttk.Label(frameStrategy, text = "Rain").grid(column = 1, sticky = (W, E))
+
+# Strategy grid variables
+extraStops = StringVar()
+softStops = StringVar()
+mediumStops = StringVar()
+hardStops = StringVar()
+rainStops = StringVar()
+extraFuel = StringVar()
+softFuel = StringVar()
+mediumFuel = StringVar()
+hardFuel = StringVar()
+rainFuel = StringVar()
+extraPitTime = StringVar()
+softPitTime = StringVar()
+mediumPitTime = StringVar()
+hardPitTime = StringVar()
+rainPitTime = StringVar()
+extraTCD = StringVar()
+softTCD = StringVar()
+mediumTCD = StringVar()
+hardTCD = StringVar()
+rainTCD = StringVar()
+extraFLD = StringVar()
+softFLD = StringVar()
+mediumFLD = StringVar()
+hardFLD = StringVar()
+rainFLD = StringVar()
+extraPitTotal = StringVar()
+softPitTotal = StringVar()
+mediumPitTotal = StringVar()
+hardPitTotal = StringVar()
+rainPitTotal = StringVar()
+extraTotal = StringVar()
+softTotal = StringVar()
+mediumTotal = StringVar()
+hardTotal = StringVar()
+rainTotal = StringVar()
+
+stops = [extraStops, softStops, mediumStops, hardStops, rainStops]
+fuels = [extraFuel, softFuel, mediumFuel, hardFuel, rainFuel]
+pitTimes = [extraPitTime, softPitTime, mediumPitTime, hardPitTime, rainPitTime]
+TCDs = [extraTCD, softTCD, mediumTCD, hardTCD, rainTCD]
+FLDs = [extraFLD, softFLD, mediumFLD, hardFLD, rainFLD]
+totals = [extraTotal, softTotal, mediumTotal, hardTotal, rainTotal]
+
+grid = [stops, fuels, pitTimes, TCDs, FLDs, totals]
+
+for stop in stops:
+	stop.set("0")
+for fuel in fuels:
+	fuel.set("0")
+for pitTime in pitTimes:
+	pitTime.set("0")
+for TCD in TCDs:
+	TCD.set("0")
+for FLD in FLDs:
+	FLD.set("0")
+for total in totals:
+	total.set("0")
+
+x = 2
+for values in grid:
+	y = 3
+	for value in values:
+		ttk.Label(frameStrategy, textvariable = value).grid(column = x, row = y, sticky = (W))
+		y = y + 1
+	x = x + 1
 
 # Automatically organize the window
-for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+for child in frameSetup.winfo_children(): child.grid_configure(padx=5, pady=5)
+for child in frameStrategy.winfo_children(): child.grid_configure(padx=5, pady=5)
 
 
 # Set some QOL things, like auto focus for text entry and how to handle an "Enter" press
