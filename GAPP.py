f939b4063a146fa8b497cbf07a8d30c04dbbf3dd
Jake Ratcliffe <JakeR1342@gmail.com>
2019-04-08 16:53:41 +0100

    Removed a lost 'print' statement

--- GAPP.py
+++ GAPP.py
@@ -131,8 +131,6 @@ def calculate(*args):
 
 			strategy = strategyCalc(username, password, wear, laps)
 
-			print(strategy)
-
 			if(strategy == 1):
 				warningLabel.set("Incorrect Login Details")
 				foregroundColour("Status.Label", "Red")
